9dfe541f25f58288eefd9fc9eb270be4c93cf2bf
DaMews <adammews@gmail.com>
2020-12-07 17:27:03 -0300

    Adding some css

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-## Getting Started
+# "YES OR NO?"
 
-First, run the development server:
+Want a YES or NO answer? Get into the app to discover [APPLINK]
+
+## About
+
+This is a project made with [Next.js](https://nextjs.org) and deployed with [Vercel Platform](https://vercel.com/import?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme).
+
+### How it works?
+
+When the user clicks the button "GET MY ANSWER!", it randoms a YES or NO answer and it shows a GIF based on that answer.
+
+### What's was used?
+
+- [Next.js](https://nextjs.org)
+- [GIPHY API](https://developers.giphy.com/docs/api#quick-start-guide)
+
+## How to run
+
+First, clone the repo into your machine:
+
+```bash
+git clone https://github.com/LiserLine/yesorno-nextjs.git yesorno-app
+
+cd yesorno-app
+```
+
+Then, run the development server:
 
 ```bash
 npm run dev
@@ -11,22 +36,3 @@ yarn dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/import?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
-# yesorno-nextjs
-An simple app to get a YES or NO answer made in Next.js
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,37 +1,39 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { useState } from 'react';
-import useSWR from 'swr';
-
-const fetcher = (url) => fetch(url).then((res) => res.json())
+import { useEffect, useState } from 'react';
 
 const Home = () => {
 
   const [imageUrl, setImageUrl] = useState('https://media.giphy.com/media/GwRBmXyEOvFtK/giphy.gif');
   const [answer, setAnswer] = useState('-');
+  const [isGettingAnswer, setIsGettingAnswer] = useState(false);
 
-  function getAnswer(){
-    
+  function getAnswer() {
+
     const options = ['YES', 'NO'];
-    const errorGifUrl = 'https://media.giphy.com/media/fV1yHo8YyoKjzvMCKr/giphy.gif'; //ERROR GIF
 
     let answer = options[(Math.floor(Math.random() * 2))];
 
     let url = `https://api.giphy.com/v1/gifs/search?q=${answer}&rating=g&api_key=${process.env.GIPHY_API_KEY}&offset=${getRandomIntInclusive(0, 50)}&limit=50`;
     fetch(url)
-        .then((resp) => resp.json())
-        .then(function (d) {
-            let randomGifUrl = d.data[getRandomIntInclusive(0,49)].images.original.url;
-            setAnswer(answer);
-            setImageUrl(randomGifUrl);
-        });
+      .then((resp) => resp.json())
+      .then(function (d) {
+        let randomGifUrl = d.data[getRandomIntInclusive(0, 49)].images.original.url;
+        setAnswer(answer);
+        setImageUrl(randomGifUrl);
+      });
+
   }
+
+  useEffect(() => {
+    setIsGettingAnswer(false);
+  })
 
   function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  }
 
   return (
     <div className={styles.container}>
@@ -40,30 +42,32 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          YES OR NO?
+      <div className={answer === '-' ? styles.noneContainer : (answer === 'YES' ? styles.yesContainer : styles.noContainer)}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            YES OR NO?
         </h1>
 
-        <p className={styles.description}>
-          Press the button to get your answer!
+          <p className={styles.description}>
+            Press the button to get your answer!
         </p>
 
-        <div>
-          <button onClick={getAnswer}>GET MY ANSWER!</button>
-        </div>
+          <div>
+            <button className={styles.btn} onClick={getAnswer} disabled={isGettingAnswer}>GET MY ANSWER!</button>
+          </div>
 
-        <div>
-          <p>
-            {answer}
-          </p>
-        </div>
+          <div>
+            <h2>
+              {answer}
+            </h2>
+          </div>
 
-        <div className={styles.grid}>
-          <img src={imageUrl}></img>
-        </div>
+          <div className={styles.grid}>
+            <img className={styles.animatedGif} src={imageUrl}></img>
+          </div>
 
-      </main>
+        </main>
+      </div>
 
       <footer className={styles.footer}>
         <a
@@ -74,6 +78,8 @@ const Home = () => {
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
+
+        <a href="https://github.com/LiserLine/yesorno-nextjs" target="_blank" className={styles.btn}>View on Github</a>
       </footer>
     </div>
   )
